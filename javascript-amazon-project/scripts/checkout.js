61954527550c4676481f0f27e5bf5f60a1fa8e56
almorena <alessio.morena@gmail.com>
2024-08-26 16:08:43 +0200

    make delivery option interact

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -103,7 +103,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId
 
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" 
+    data-option-id=${matchingProduct.Id}
+    data-delivery-option-id=${deliveryOption.id}
+    >
       <input type="radio"
         ${isChecked ? 'checked': ''}
         class="delivery-option-input"
@@ -136,3 +139,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   })
   
 })
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    });
+  });
